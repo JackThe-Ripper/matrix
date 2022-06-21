c7d8f6abb7401fc1593112cf67eedc5dd52af868
JackThe-Ripper <107028170+JackThe-Ripper@users.noreply.github.com>
2022-06-21 11:45:27 +0300

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,20 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8 ,9],
+    [10, 11, 12]
+]
+print('Строки матрицы')
+for stroki in matrix:
+    print(stroki)
+    
+print('Столбцы матрицы')
+print('Первый столбец')
+for stolbsi in matrix:
+    print(stolbsi[0])
+print('Второй столбец')
+for stolbsi in matrix:
+    print(stolbsi[1])
+print('Третий столбец')
+for stolbsi in matrix:
+    print(stolbsi[2])
